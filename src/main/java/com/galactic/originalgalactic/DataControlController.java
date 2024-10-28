318fbe2e2b9c5688cf3977f8c97de329dcaa5903
RajveerBiradar <rajveerbiradar360@gmail.com>
2024-10-28 05:32:49 +0530

    icons added

--- src/main/java/com/galactic/originalgalactic/DataControlController.java
+++ src/main/java/com/galactic/originalgalactic/DataControlController.java
@@ -1,5 +1,7 @@
 package com.galactic.originalgalactic;
 
+import javafx.fxml.FXML;
+
 public class DataControlController {
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.galactic.originalgalactic {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.galactic.originalgalactic to javafx.fxml;
